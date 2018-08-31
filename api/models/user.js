3704e8ba2c4bec652e14ace643d745720c841e0c
Willdta <beyondutraining@gmail.com>
2018-08-31 19:32:49 -0400

    fixed deprecation warning

--- api/models/user.js
+++ api/models/user.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 
-const userSchema = mongoose.Schema({
+// fixed deprecation warning
+mongoose.set('useCreateIndex', true)
+
+  const userSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   email: {
     type: String,
