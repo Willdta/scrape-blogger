ad5dd695794e9a4f46148b8b3ca1091fcc69fade
Yves Middleton <toks@Yvess-MacBook-Pro.local>
2018-09-24 17:18:10 -0400

    added inital form with on change event

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('express')()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
